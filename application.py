7ba3846b83ebf474d3700696fff826a857e750c4
robert791223 <a0955751486@gmail.com>
2021-05-02 11:40:09 +0800

    flask:hello_world_debug

--- application.py
+++ application.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods='GET')
-der hello_world():
+@app.route('/', methods=['GET'])
+def hello_world():
     return "Hello World"
 
 
